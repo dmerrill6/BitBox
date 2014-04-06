5648494b7ffc4d758b629a43ea4c5aecd9448848
Daniel <macbook@MacBook-Pro-de-macbook-3.local>
2014-04-06 09:50:31 -0300

    tooltips pre-alpha

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 
-function getAddressInformation(bitcoinAddress){
+function getAddressInformation(bitcoinAddress, callbackFunction){
 	//Use insight.is web services to get the information
 	var url = "http://live.insight.is/api/addr/" + bitcoinAddress;
 	//send ajax request
@@ -7,11 +7,38 @@ function getAddressInformation(bitcoinAddress){
 		type: "GET", 
 		url: url,
 	}).done(function(response){
-		 callback(response);
+		 callbackFunction(response);
 	});
 }
-function callback(msg){
-	console.log(msg);
+
+function addTooltip(target, bitcoinAddress){
+	target.mouseenter(function (e) { //event fired when mouse cursor enters target
+	    var $x = e.pageX - this.offsetLeft; //get mouse X coordinate relative to target element
+ 		if(target.attr('tooltipappended') != 'true')
+        	$(this).append('<div class="tooltip"></div>'); //append tooltip markup
+ 		target.attr('tooltipappended', 'true');
+
+ 		//Function to insert content in the tooltip
+ 		function setContent(msg){
+			console.log(msg);
+			
+			$('.tooltip').append(msg);
+
+		}
+		//Get bitcoin address information
+		getAddressInformation(bitcoinAddress, setContent);
+ 
+        $("a > div.tooltip.center").css("left", "" + $x - 103 + "px"); //set tooltip position from left
+        $("a > div.tooltip.left").css("left", "" + $x + "px"); //set tooltip position from left
+        $("a > div.tooltip.right").css("left", "" + $x - 208 + "px"); //set tooltip position from left
+ 
+        $("a > div.tooltip").fadeIn(300); //display, animate and fade in the tooltip
+	});
+	target.mouseleave(function (e){
+		$("a > div.tooltip").hide(); //fadeout the tooltip
+	});
 }
 
-getAddressInformation("1C42E62XCkG6WtZ1y1YVVbxQTNn7kHXU6K");
+var target = $('a');
+var address = "1C42E62XCkG6WtZ1y1YVVbxQTNn7kHXU6K";
+addTooltip(target, address);

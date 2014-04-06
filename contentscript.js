3dc580d7eba4a71cefb5849f3c3c15bd23c424cb
Leo Wandersleb <Leo-Wandersleb-bcontext@leowandersleb.de>
2014-04-06 15:38:28 -0300

    added addresses that start with 3 as candidates and changed validation to isValid()

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,14 @@
 var b = $("body").html();
 var Address = require('bitcore').Address;
 var regexNonTag = /\>[^\<]*\</gi; //outside of html tags
-var regexBTC = /1[^ ]{33}/gi; //bitcoin address candidate
+var regexBTC = /[13][^ ^\>^\<]{33}/gi; //bitcoin address candidate
 b = b.replace(regexNonTag, function(m1) {
 		return m1.replace(regexBTC, function(m2) {
 			var addressCandidate =  new Address(m2);
 			console.log(addressCandidate);
-		    if (addressCandidate.data =! undefined){
-				  return m2 + "<div class='bitchrome-address' address='"+ m2 + "'/>";
+		    if (addressCandidate.isValid()) {
+				console.log("is address. m2 is " + m2);
+				return m2 + "<div class='bitchrome-address' address='"+ m2 + "'/>";
 		    }
 		    else{
 		    	return m2;

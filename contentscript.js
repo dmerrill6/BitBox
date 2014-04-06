f3ddf00a494ed769de050941072cd77861ecba58
Daniel <macbook@MacBook-Pro-de-macbook-3.local>
2014-04-06 10:54:10 -0300

    merge

--- contentscript.js
+++ contentscript.js
@@ -3,6 +3,7 @@ var b = $("body").html();
 
 var regexNonTag = /\>[^\<]*\</gi; //outside of html tags
 var regexBTC = /1[^ ]{33}/gi; //bitcoin address candidate
+console.log(b);
 b = b.replace(regexNonTag, function(m1) {
 		return m1.replace(regexBTC, function(m2) {
 			return '<div style="height:5em;background:red" onClick="getAddressInformation(\''+m2+'\')">'+m2+'</div>';

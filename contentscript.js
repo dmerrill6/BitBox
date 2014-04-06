cee49f03ae987c3fadaca37a1aef6742c29a7ff7
Daniel <macbook@MacBook-Pro-de-macbook-3.local>
2014-04-06 11:20:56 -0300

    append bitcoin logo to address in content_script

--- contentscript.js
+++ contentscript.js
@@ -1,28 +1,12 @@
-function getAddressInformation(bitcoinAddress){
-	//Use insight.is web services to get the information
-	var url = "http://live.insight.is/api/addr/" + bitcoinAddress;
-	//send ajax request
-	$.ajax({
-		type: "GET", 
-		url: url,
-	}).done(function(response){
-		 callback(response);
-	});
-}
-
-function callback(msg){
-	console.log(msg);
-}
-
+var b = $("body").html();
+var Address = require('bitcore').Address;
 var regexNonTag = /\>[^\<]*\</gi; //outside of html tags
 var regexBTC = /1[^ ]{33}/gi; //bitcoin address candidate
-console.log(b);
 b = b.replace(regexNonTag, function(m1) {
 		return m1.replace(regexBTC, function(m2) {
-			var Address = require('bitcore').Address;
 			var addressCandidate =  new Address(m2);
 		    if (addr.validate()){
-				return '<div style="height:5em;background:red" onClick="getAddressInformation(\''+m2+'\')">'+m2+'</div>';
+				  return m2 + "<div class='bitchrome-address' address='"+ m2 + "'/>";
 		    }
 		    else{
 		    	return m2;

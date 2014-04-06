5ed4d07f184ffc84c9a37cebf3880325ee6c1f83
Leo Wandersleb <Leo-Wandersleb-bcontext@leowandersleb.de>
2014-04-06 01:32:49 -0300

    somehow bitcoin addresses get into divs now

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,28 @@
-var regex = /1../gi;
-matches = document.body.innerText.match(regex);
-if (matches) {
-  for(i=0; i<matches.length; i++ ) {
-    console.log("hi " + matches[i]);
-  }
-  // mess with the page:
-  $("p").hide(400, function(){$(this).show(400)});
-  // document.body.style.backgroundColor="red";
+var b = $("body").html();
+
+function getAddressInformation(bitcoinAddress){
+	//Use insight.is web services to get the information
+	var url = "http://live.insight.is/api/addr/" + bitcoinAddress;
+	//send ajax request
+	$.ajax({
+		type: "GET", 
+		url: url,
+	}).done(function(response){
+		 callback(response);
+	});
 }
+
+function callback(msg){
+	console.log(msg);
+}
+
+var regexNonTag = /\>[^\<]*\</gi; //outside of html tags
+var regexBTC = /1[^ ]{33}/gi; //bitcoin address candidate
+console.log(b);
+b = b.replace(regexNonTag, function(m1) {
+		return m1.replace(regexBTC, function(m2) {
+			return '<div style="height:5em;background:red" onClick="getAddressInformation(\''+m2+'\')">'+m2+'</div>';
+		});
+	});
+$("body").html(b);
+

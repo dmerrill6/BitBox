e28907a1ab5d9761fe92d19f4003d636ef885a4f
trueke <trueke@samsung>
2014-04-06 10:53:30 -0300

    Add validation of bitcore for address bitcoin

--- contentscript.js
+++ contentscript.js
@@ -21,7 +21,14 @@ var regexBTC = /1[^ ]{33}/gi; //bitcoin address candidate
 console.log(b);
 b = b.replace(regexNonTag, function(m1) {
 		return m1.replace(regexBTC, function(m2) {
-			return '<div style="height:5em;background:red" onClick="getAddressInformation(\''+m2+'\')">'+m2+'</div>';
+			var Address = require('bitcore').Address;
+			var addressCandidate =  new Address(m2);
+		    if (addr.validate()){
+				return '<div style="height:5em;background:red" onClick="getAddressInformation(\''+m2+'\')">'+m2+'</div>';
+		    }
+		    else{
+		    	return m2;
+		    }
 		});
 	});
 $("body").html(b);
